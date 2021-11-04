8134304eeba7769c28c5bd7dd82fa63226092e35
mrfreddi007 <71372874+mrfreddi007@users.noreply.github.com>
2021-11-04 12:23:44 +0100

    Update Trivial_Pursuit.py

--- Trivial_Pursuit.py
+++ Trivial_Pursuit.py
@@ -5,36 +5,6 @@ from pwn import *
 import base64
 import morse_talk as mtalk
 
-ENGLISH_TO_MORSE = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
-                    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
-                    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
-                    'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
-                    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'}
-
-# Generate MORSE_TO_ENGLISH from ENGLISH_TO_MORSE
-MORSE_TO_ENGLISH = {}
-for key, value in ENGLISH_TO_MORSE.items():
-    MORSE_TO_ENGLISH[value] = key
-
-
-def english_to_morse(message):
-    morse = []  # Will contain Morse versions of letters
-    for char in message:
-        if char in ENGLISH_TO_MORSE:
-            morse.append(ENGLISH_TO_MORSE[char])
-    return " ".join(morse)
-
-
-def morse_to_english(message):
-    message = message.split(" ")
-    english = []  # Will contain English versions of letters
-    for code in message:
-        if code in MORSE_TO_ENGLISH:
-            english.append(MORSE_TO_ENGLISH[code])
-    return "".join(english)
-
-
-
 
 #Connect with netcat
 io = connect("io.ept.gg", 30023)
